0a41b31e7e0e0906ba81afac3769dd2489d20180
Lucas F Mancan <lucasfmancan@hotmail.com>
2023-11-05 11:36:14 -0300

    printing a simple banner on server start event

--- src/main/java/org/manz/ManzServer.java
+++ src/main/java/org/manz/ManzServer.java
@@ -4,6 +4,8 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.concurrent.Executor;
@@ -29,6 +31,7 @@ public class ManzServer {
     }
 
     public void start() {
+        printApplicationBanner();
         try (ServerSocket serverSocket = new ServerSocket(port)) {
             while (true) {
                 Socket client = serverSocket.accept();
@@ -36,6 +39,23 @@ public class ManzServer {
             }
         } catch (IOException exception) {
             logger.error("Internal error running Manz server", exception);
+        }
+    }
+
+    private void printApplicationBanner() {
+        try (InputStream is = getClass().getClassLoader().getResourceAsStream("banner.txt")) {
+            try (var fileReader = new InputStreamReader(is)) {
+
+                var buffer = new char[is.available()];
+                var read = fileReader.read(buffer);
+
+                var bannerString = new String(buffer);
+
+                System.out.println(bannerString);
+
+            }
+        } catch (Exception e) {
+            logger.debug("Error loading application banner", e);
         }
     }
 }
